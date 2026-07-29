12b1b9e4e92dcb469a5cdd0da17db6a168d93648
MujahidulIslam4541 <mirifat4541@gmail.com>
2026-07-29 21:30:15 +0600

    completed second query

--- query.sql
+++ query.sql
@@ -1,4 +1,9 @@
--- Query 1: Champions League matches that are Available
+// Query 1 
 SELECT match_id, fixture, base_ticket_price
 FROM matches
 WHERE tournament_category = 'Champions League' AND match_status = 'Available';
+
+// Query 2 
+SELECT user_id, full_name, email
+FROM users
+WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
