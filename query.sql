419606651e134c5be0187ab6f5ad3e86aedfd271
MujahidulIslam4541 <mirifat4541@gmail.com>
2026-07-29 21:46:05 +0600

    completed 3rd and 4th query

--- query.sql
+++ query.sql
@@ -7,3 +7,17 @@ WHERE tournament_category = 'Champions League' AND match_status = 'Available';
 SELECT user_id, full_name, email
 FROM users
 WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
+
+
+// Query 3
+SELECT booking_id, user_id, match_id,
+       COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM bookings
+WHERE payment_status IS NULL;
+
+
+// Query 4
+SELECT b.booking_id, u.full_name, m.fixture, b.total_cost
+FROM bookings b
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN matches m ON b.match_id = m.match_id;
